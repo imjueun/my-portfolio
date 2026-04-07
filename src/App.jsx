4c7ab73f52c90c1e6b2608c2d88762f0fd884f49
Jueun Lee <142564905+imjueun@users.noreply.github.com>
2026-04-07 15:14:20 +0900

    UPDATE: about 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import About from './pages/About';
-import Skills from './pages/Skills';
 
 export default function App() {
   return (
-    /* w-full: 너비 100%
-       overflow-x-hidden: 가로 스크롤 방지
-       p-0: 모든 패딩 제거 
-    */
     <div className="w-full min-h-screen p-0 m-0 overflow-x-hidden bg-[#fafafa]">
       <About />
-      <Skills />
     </div>
   );
 }
